7ce82f342e021710427b7a90a94d0e6fd73161ad
jinseok1006 <jinseok1006@naver.com>
2023-06-02 20:39:23 +0900

    update

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
-import path from 'path';
 import express from 'express';
+import helmet from 'helmet';
+import type { Request, Response } from 'express';
+
+import { contains } from './util';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,52 +12,51 @@ interface rank {
   sec: number;
 }
 
+const method = ['easy', 'normal', 'hard'] as const;
+
 const store = {
   easy: [] as rank[],
   normal: [] as rank[],
   hard: [] as rank[],
 };
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.json(store);
 });
 
-app.get('/get/:diff', (req: express.Request, res: express.Response) => {
+app.get('/get/:diff', (req: Request, res: Response) => {
   const diff = req.params.diff as string;
 
-  // 외부 함수를 통한 type guard가 불가능...
-  if (!(diff === 'easy' || diff === 'normal' || diff === 'hard'))
-    return void res.send(false);
+  if (!contains(method, diff)) return void res.send(false);
 
-  res.send(store[diff]);
+  res.json(store[diff]);
 });
 
-app.get('/register/:diff', (req: express.Request, res: express.Response) => {
-  const diff = req.params.diff;
-  if (!(diff === 'easy' || diff === 'normal' || diff === 'hard'))
-    return void res.send(false);
+app.get('/register/:diff', (req: Request, res: Response) => {
+  const diff = req.params.diff as string;
 
-  if (!req.query) return void res.send(false);
-  if (typeof req.query.name !== 'string' || typeof req.query.sec !== 'string')
+  if (!contains(method, diff)) return void res.send(false);
+  if (!req.query || !req.query.name || !req.query.sec)
     return void res.send(false);
 
   const rank = {
-    name: req.query.name,
-    sec: parseFloat(req.query.sec),
+    name: req.query.name as string,
+    sec: parseFloat(req.query.sec as string),
   };
 
   store[diff].push(rank);
   store[diff].sort((a, b) => a.sec - b.sec);
 
-  console.log(store[diff]);
+  console.log(`ip:${req.ip}, rank: ${rank}`);
 
   res.send(true);
 });
 
-app.get('/reset', (req: express.Request, res: express.Response) => {
+app.get('/reset', (req: Request, res: Response) => {
   store.easy.splice(0);
   store.normal.splice(0);
   store.hard.splice(0);
